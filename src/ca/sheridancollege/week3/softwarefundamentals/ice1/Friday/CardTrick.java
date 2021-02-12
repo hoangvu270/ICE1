944f9d1476c4893adf1e71f145aded446eb7b377
Hoang Vu <vuhoan@sheridancollege.ca>
2021-02-12 17:16:27 -0500

    Created cardA - an object of Card class in Cadtrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * @modifier Hoang Vu
+ * Student number 991623740
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card cardA = new Card();
     }
     
 }
